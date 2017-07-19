81b1865b0b25adb729f76ec7c4f01e50ce95570f
jchegenye <chegenyejackson@gmail.com>
2017-07-19 21:47:50 +0300

    Coming soon page

---
+++ js/background.cycle-custom.js
@@ -0,0 +1,31 @@
+// JavaScript Document
+
+var fadeShow = $(".background").fadeShow({
+
+
+
+	correctRatio: true,
+
+
+
+	shuffle: true,
+
+
+
+	speed: 2500,
+
+
+
+	images: ['images/bg-image.png',
+
+
+
+			 'images/bg-image-2.png']
+
+
+
+
+
+
+
+});
---
+++ js/custom.js
@@ -0,0 +1,144 @@
+// JavaScript Document
+
+$('#subscribeform').submit(function(){
+
+
+
+	var action = $(this).attr('action');
+
+
+
+		$("#mesaj").slideUp(750,function() {
+
+
+
+
+
+
+
+		$('#mesaj').hide();
+
+
+
+	$('#subsubmit')
+
+
+
+
+
+
+
+			.after('')
+
+
+
+
+
+
+
+			.attr('disabled','disabled');
+
+
+
+	$.post(action, {
+
+
+
+
+
+
+
+			email: $('#subemail').val(),
+			subnames: $('#subnames').val()
+
+
+
+
+
+
+
+		},
+
+
+
+
+
+
+
+			function(data){
+
+
+
+
+
+
+
+				document.getElementById('mesaj').innerHTML = data;
+
+
+
+
+
+
+
+				$('#mesaj').slideDown('slow');
+
+
+
+
+
+
+
+				$('#subscribeform img.subscribe-loader').fadeOut('slow',function(){$(this).remove()});
+
+
+
+
+
+
+
+				$('#subsubmit').removeAttr('disabled');
+
+
+
+
+
+
+
+				if(data.match('success') != null) $('#subscribeform').slideUp('slow');
+
+
+
+			}
+
+
+
+
+
+
+
+		);
+
+
+
+
+
+
+
+		});
+
+
+
+
+
+
+
+		return false;
+
+
+
+
+
+
+
+	});
